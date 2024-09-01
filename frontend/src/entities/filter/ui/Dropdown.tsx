c4e4a6c8d0c523795a04066547df491b1f342bf3
youlimsongs <giuhki@gmail.com>
2024-09-02 03:41:59 +0900

    feat: 프로젝트 종류 필터링 삭제

--- frontend/src/entities/filter/ui/Dropdown.tsx
+++ frontend/src/entities/filter/ui/Dropdown.tsx
@@ -1,7 +1,6 @@
 import { SetStateAction } from 'react';
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
-import iconDropdown from '../image/Icon-Dropdown.png';
 
 type DropDownProps = {
   selectedType: string;
@@ -19,29 +18,17 @@ type DropdownMenuProps = {
 
 export function DropDown({
   selectedType,
-  setSelectedType,
   selectedYear,
   setSelectedYear,
   selectedPeriod,
   setSelectedPeriod,
 }: DropDownProps) {
-  const firstDropdown = ['전체', '부트캠프', '팀 프로젝트', '개인 프로젝트'];
+  // const firstDropdown = ['전체', '부트캠프', '팀 프로젝트', '개인 프로젝트'];
   const secondDropdown = ['전체', '2022', '2023', '2024'];
   const bootCampDropdown = ['전체', '동계', '하계'];
 
-  // 첫 번째 드롭다운의 상태값이 바뀌면 두 번째 드롭다운의 상태값이 초기값으로 바뀜
-  const handleFirstDropdownChange = (option: SetStateAction<string>) => {
-    setSelectedType(option);
-    setSelectedPeriod('전체');
-  };
-
   return (
-    <div className="space-x-4 mb-8 flex justify-center">
-      <DropdownMenu
-        options={firstDropdown}
-        selectedOption={selectedType}
-        setSelectedOption={handleFirstDropdownChange}
-      />
+    <div className="space-x-4 flex justify-center">
       <DropdownMenu options={secondDropdown} selectedOption={selectedYear} setSelectedOption={setSelectedYear} />
       {selectedType === '부트캠프' ? (
         <DropdownMenu
@@ -58,11 +45,20 @@ export function DropDown({
 
 function DropdownMenu({ options, selectedOption, setSelectedOption }: DropdownMenuProps) {
   return (
-    <Menu as="div" className="relative inline-block text-left font-['Pretendard-Thin']">
+    <Menu as="div" className="relative inline-block text-left font-['Pretendard'] font-[400]">
       <div>
-        <Menu.Button className="w-[10rem] inline-flex justify-between rounded-md bg-[#111111] px-4 py-2 text-sm text-[#888888] shadow-sm ring-1 ring-inset ring-[#444444] hover:bg-[#242628] hover:text-[#cccccc]">
+        <Menu.Button className="w-[9rem] inline-flex justify-between rounded-xl bg-[#cccccc15] px-4 py-3 text-[1rem] text-[#cccccc] shadow-sm hover:bg-[#242628] hover:text-[#cccccc]">
           {selectedOption}
-          <img src={iconDropdown} className="w-[0.625rem] h-[0.375rem] self-center ml-auto" alt="Dropdown Icon" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 15"
+            strokeWidth="1.6"
+            stroke="currentColor"
+            className="size-4"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+          </svg>
         </Menu.Button>
       </div>
       <Transition
@@ -74,7 +70,7 @@ function DropdownMenu({ options, selectedOption, setSelectedOption }: DropdownMe
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="w-[10rem] absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-[#111111] shadow-lg ring-1 ring-[#444444] focus:outline-none">
+        <Menu.Items className="w-[9rem] absolute right-0 z-10 mt-2 origin-top-right rounded-xl bg-[#222222] shadow-lg ring-1 ring-[#444444] focus:outline-none">
           <div className="py-1">
             {options.map((option) => (
               <Menu.Item key={option}>
@@ -82,8 +78,8 @@ function DropdownMenu({ options, selectedOption, setSelectedOption }: DropdownMe
                   <button
                     onClick={() => setSelectedOption(option)}
                     className={`${
-                      active ? 'bg-[#242628] text-[#cccccc]' : 'text-[#888888]'
-                    } block w-full px-4 py-2 text-left text-sm`}
+                      active ? 'bg-[#3d3d3d] rounded-lg text-[#cccccc]' : 'text-[#888888]'
+                    } block w-full px-4 py-2 text-left text-[1rem]`}
                   >
                     {option}
                   </button>
