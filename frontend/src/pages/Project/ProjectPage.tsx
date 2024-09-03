cb6acffa53beeaf26a065af4b04e11a796f1dc0c
youlimsongs <giuhki@gmail.com>
2024-09-03 17:25:29 +0900

    feat: 프로젝트 페이지 디자인 변경

--- frontend/src/pages/Project/ProjectPage.tsx
+++ frontend/src/pages/Project/ProjectPage.tsx
@@ -14,11 +14,8 @@ export const ProjectPage = () => {
     <div className="w-screen bg-[#111111] flex flex-col justify-center items-center">
       <NavBar />
       <div className="flex flex-col items-center justify-center py-[4.5rem] font-['Pretendard-Bold'] text-[3rem] text-white"></div>
-      <div className="flex flex-col box-sizing-border items-start justify-center gap-[3.5rem] w-[1200px] px-4 mt-8 my-6 font-['Pretendard]">
+      <div className="flex flex-col box-sizing-border items-start justify-center gap-[3.5rem] w-[1200px] px-4 mt-8 my-4 font-['Pretendard]">
         <ProjectTabs selectedType={selectedType} setSelectedType={setSelectedType} setSelectedYear={setSelectedYear} />
-        <div className="flex flex-col items-start justify-center text-[1.2rem] px-[0.5rem] mb-[2rem] font-[500] text-[#cccccc]">
-          8주 동안 프로젝트의 기획부터 배포까지 진행하는 테커의 부트캠프 프로젝트를 소개합니다.
-        </div>
         <DropDown
           selectedType={selectedType}
           setSelectedType={setSelectedType}
