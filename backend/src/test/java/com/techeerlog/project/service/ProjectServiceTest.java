9d1fd6eb17524fc95d15c597982013877ece583e
김주언 <wndjs803@naver.com>
2024-09-30 17:29:30 +0900

    test: findResponse 테스트 코드 작성

---
+++ backend/src/test/java/com/techeerlog/project/service/ProjectServiceTest.java
@@ -0,0 +1,181 @@
+package com.techeerlog.project.service;
+
+import com.techeerlog.auth.dto.AuthInfo;
+import com.techeerlog.framework.repository.FrameworkRepository;
+import com.techeerlog.global.mapper.FrameworkMapper;
+import com.techeerlog.global.mapper.MemberMapper;
+import com.techeerlog.global.mapper.ProjectMapper;
+import com.techeerlog.global.support.UtilMethod;
+import com.techeerlog.love.domain.Love;
+import com.techeerlog.love.repository.LoveRepository;
+import com.techeerlog.member.domain.LoginId;
+import com.techeerlog.member.domain.Member;
+import com.techeerlog.member.domain.Nickname;
+import com.techeerlog.member.domain.Password;
+import com.techeerlog.member.repository.MemberRepository;
+import com.techeerlog.project.domain.Project;
+import com.techeerlog.project.dto.ProjectResponse;
+import com.techeerlog.project.enums.*;
+import com.techeerlog.project.repository.NonRegisterProjectMemberRepository;
+import com.techeerlog.project.repository.ProjectFrameworkRepository;
+import com.techeerlog.project.repository.ProjectMemberRepository;
+import com.techeerlog.project.repository.ProjectRepository;
+import com.techeerlog.scrap.domain.Scrap;
+import com.techeerlog.scrap.repository.ScrapRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.awt.print.PrinterJob;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class ProjectServiceTest {
+    @Mock
+    private ProjectRepository projectRepository;
+    @Mock
+    private UtilMethod utilMethod;
+    @Mock
+    private ProjectMemberRepository projectMemberRepository;
+    @Mock
+    private MemberRepository memberRepository;
+    @Mock
+    private FrameworkRepository frameworkRepository;
+    @Mock
+    private ProjectFrameworkRepository projectFrameworkRepository;
+    @Mock
+    private NonRegisterProjectMemberRepository nonRegisterProjectMemberRepository;
+    @Mock
+    private LoveRepository loveRepository;
+    @Mock
+    private ScrapRepository scrapRepository;
+    @Spy
+    private final ProjectMapper projectMapper = Mappers.getMapper(ProjectMapper.class);
+    @Spy
+    private final MemberMapper memberMapper = Mappers.getMapper(MemberMapper.class);
+    @Spy
+    private final FrameworkMapper frameworkMapper = Mappers.getMapper(FrameworkMapper.class);
+    @InjectMocks
+    private ProjectService projectService;
+
+    @Nested
+    class findProjectResponseTest {
+        @Test
+        @DisplayName("프로젝트 단일 조회")
+        void findProjectResponseTestSuccess() {
+            // given
+            Member member = createMember(1L);
+            Project project = createProject(1L, member);
+            Love love = createLove(member, project);
+            Scrap scrap = createScrap(member, project);
+
+            Mockito.when(projectRepository.findById(any()))
+                    .thenReturn(Optional.ofNullable(project));
+            Mockito.when(loveRepository.findByMemberIdAndProjectId(any(), any()))
+                    .thenReturn(Optional.ofNullable(love));
+            Mockito.when(scrapRepository.findByMemberIdAndProjectId(any(), any()))
+                    .thenReturn(Optional.ofNullable(scrap));
+
+            // when
+            ProjectResponse projectResponse =
+                    projectService.findProjectResponse(1L,
+                            new AuthInfo(1L, "type", "test"));
+
+            // then
+            Assertions.assertEquals(projectResponse.getId(), 1L);
+            Assertions.assertEquals(projectResponse.getProjectMemberResponseList().size(), 0);
+            Assertions.assertTrue(projectResponse.isLoved());
+            Assertions.assertTrue(projectResponse.isScraped());
+        }
+
+        private Member createMember(Long memberId) {
+            return Member.builder()
+                    .id(memberId)
+                    .introduction("instruction" + memberId)
+                    .loginId(new LoginId("test" + memberId))
+                    .nickname(new Nickname("test" + memberId))
+                    .password(new Password("1234"))
+                    .profileImageUrl("profileImageUrl" + memberId)
+                    .build();
+        }
+
+        private Project createProject(Long projectId, Member member) {
+            return Project.builder()
+                    .id(projectId)
+                    .title("title" + projectId)
+                    .subtitle("subtitle" + projectId)
+                    .mainImageUrl("mainImageUrl" + projectId)
+                    .content("content" + projectId)
+                    .startDate(LocalDate.now())
+                    .endDate(LocalDate.now())
+                    .blogLink("blogLink" + projectId)
+                    .githubLink("githubLink" + projectId)
+                    .websiteLink("websiteLink" + projectId)
+                    .commentList(Collections.emptyList())
+                    .projectFrameworkList(Collections.emptyList())
+                    .projectMemberList(Collections.emptyList())
+                    .nonRegisterProjectMemberList(Collections.emptyList())
+                    .loveList(Collections.emptyList())
+                    .projectStatusEnum(ProjectStatusEnum.COMPLETED)
+                    .projectTypeEnum(ProjectTypeEnum.BOOTCAMP)
+                    .platform(PlatformEnum.WEB)
+                    .projectTeamNameEnum(ProjectTeamNameEnum.A)
+                    .rankEnum(RankEnum.FIRST)
+                    .year(2024)
+                    .semesterEnum(SemesterEnum.ALL)
+                    .member(member)
+                    .build();
+        }
+
+        private Love createLove(Member member, Project project) {
+            return Love.builder()
+                    .project(project)
+                    .member(member)
+                    .build();
+        }
+
+        private Scrap createScrap(Member member, Project project) {
+            return Scrap.builder()
+                    .project(project)
+                    .member(member)
+                    .build();
+        }
+
+//        private ProjectResponse createProjectResponse(
+//                Long projectResponseId,
+//                Project project) {
+//            return ProjectResponse.builder()
+//                    .id(projectResponseId)
+//                    .mainImageUrl(project.getMainImageUrl())
+//                    .title(project.getTitle())
+//                    .subtitle(project.getSubtitle())
+//                    .content(project.getContent())
+//                    .startDate(project.getStartDate())
+//                    .endDate(project.getEndDate())
+//                    .platform(project.getPlatform())
+//                    .projectTypeEnum(project.getProjectTypeEnum())
+//                    .projectTeamNameEnum(project.getProjectTeamNameEnum())
+//                    .year(project.getYear())
+//                    .semesterEnum(project.getSemesterEnum())
+//                    .projectStatusEnum(project.getProjectStatusEnum())
+//                    .githubLink(project.getGithubLink())
+//                    .blogLink(project.getBlogLink())
+//                    .websiteLink(project.getWebsiteLink())
+//                    .loveCount(0)
+//                    .isLoved(true)
+//                    .isScraped(true)
+//                    .build();
+//        }
+    }
+
+}
