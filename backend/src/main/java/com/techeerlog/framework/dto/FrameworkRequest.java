0f611b1d8a41ecf09a7c4cf4b7ee23ab3d31e6dc
김주언 <wndjs803@naver.com>
2024-11-26 16:10:38 +0900

    fix: @Setter 어노테이션 @Builder로 교체

--- backend/src/main/java/com/techeerlog/framework/dto/FrameworkRequest.java
+++ backend/src/main/java/com/techeerlog/framework/dto/FrameworkRequest.java
@@ -2,12 +2,12 @@ package com.techeerlog.framework.dto;
 
 import com.techeerlog.framework.enums.FrameworkTypeEnum;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class FrameworkRequest {
