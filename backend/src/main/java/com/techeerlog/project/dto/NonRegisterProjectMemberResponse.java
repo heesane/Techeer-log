cb8e4fc352cd2bc27491747470d8bdc39aea3ba7
김주언 <wndjs803@naver.com>
2024-11-26 15:37:45 +0900

    feat: @Builder 어노테이션 추가

--- backend/src/main/java/com/techeerlog/project/dto/NonRegisterProjectMemberResponse.java
+++ backend/src/main/java/com/techeerlog/project/dto/NonRegisterProjectMemberResponse.java
@@ -2,10 +2,13 @@ package com.techeerlog.project.dto;
 
 import com.techeerlog.project.enums.ProjectMemberTypeEnum;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NonRegisterProjectMemberResponse {
