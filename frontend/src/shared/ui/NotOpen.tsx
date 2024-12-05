6cf732279697d7de25359d598d2eaabf6efb442a
박준서 <jswing@catholic.ac.kr>
2024-12-05 16:44:05 +0900

    feat : 404 페이지 수정 및 NotOpen 페이지 추가

---
+++ frontend/src/shared/ui/NotOpen.tsx
@@ -0,0 +1,16 @@
+import Lottie from 'react-lottie-player';
+import sleeping from '../assets/image/error/sleeping.json';
+
+const NotOpen = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-white bg-black">
+      <Lottie style={{ width: 300, height: 300 }} loop animationData={sleeping} play />
+      <div className="flex flex-col">
+        <p className="text-2xl font-bold mb-[1rem]">지금은 운영 시간이 아닙니다.</p>
+        <p className="text-[1rem]">운영 시간은 03:00 ~ 8:00 입니다.</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotOpen;
